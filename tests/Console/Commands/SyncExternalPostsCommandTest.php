a3229c655b58dc61a498445d4e8ee708264f5649
Vitaliy Semenov <synthex@bk.ru>
2024-05-01 21:13:45 +0500

    feat: handcrafted mocks

---
+++ tests/Console/Commands/SyncExternalPostsCommandTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Console\Commands;
+
+use App\Models\ExternalPost;
+use Tests\Fakes\RssRepositoryFake;
+use Tests\TestCase;
+
+class SyncExternalPostsCommandTest extends TestCase
+{
+    public function test_sync_several_repositories_at_once(): void
+    {
+        RssRepositoryFake::setUp();
+
+        $urls = [
+            'https://test-a.com',
+            'https://test-b.com',
+            'https://test-c.com',
+        ];
+
+        config()->set('services.external_feeds', $urls);
+
+        $this->artisan('sync:externals')
+            ->assertExitCode(0);
+
+        $this->assertEquals($urls, RssRepositoryFake::getUrls());
+
+        $this->assertDatabaseCount(ExternalPost::class, 3);
+    }
+}
