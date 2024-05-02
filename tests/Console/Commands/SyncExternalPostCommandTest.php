5b560afb393846f6a314a279e5091e7f7cc43ca0
Vitaliy Semenov <synthex@bk.ru>
2024-05-02 11:15:15 +0500

    feat: testing Http facade

--- tests/Console/Commands/SyncExternalPostCommandTest.php
+++ tests/Console/Commands/SyncExternalPostCommandTest.php
@@ -7,6 +7,7 @@ use App\Models\ExternalPost;
 use App\Support\Rss\RssEntry;
 use App\Support\Rss\RssRepository;
 use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Http;
 use Mockery\MockInterface;
 use Tests\TestCase;
 
@@ -34,5 +35,79 @@ class SyncExternalPostCommandTest extends TestCase
             'url' => 'https://test.com',
             'title' => 'test',
         ]);
+    }
+
+    public function test_rss_entries_are_stored_in_the_database_without_mock(): void
+    {
+        Http::fake([
+            'https://test.com/*' => Http::sequence()
+                ->push($this->getFeed('test-a'), 200)
+                ->push($this->getFeed('test-b'), 200),
+        ]);
+
+        $sync = app(SyncExternalPost::class);
+
+        $sync('https://test.com/feed');
+
+        $this->assertDatabaseHas(ExternalPost::class, [
+            'url' => 'https://test.com/blog/test',
+            'title' => 'test-a',
+        ]);
+
+        $this->assertDatabaseMissing(ExternalPost::class, [
+            'url' => 'https://test.com/blog/test',
+            'title' => 'test-b',
+        ]);
+
+        $sync('https://test.com/feed');
+
+        $this->assertDatabaseHas(ExternalPost::class, [
+            'url' => 'https://test.com/blog/test',
+            'title' => 'test-b',
+        ]);
+    }
+
+    private function getFeed(string $title = 'test'): string
+    {
+        return  <<<XML
+       <feed xmlns="http://www.w3.org/2005/Atom">
+           <id>https://test.com/rss</id>
+           <link href="https://test.com/rss"/>
+           <title><![CDATA[ https://test.com ]]></title>
+           <updated>2021-08-11T11:00:30+00:00</updated>
+
+           <entry>
+               <title><![CDATA[$title]]></title>
+
+               <link rel="alternate" href="https://test.com/blog/test"/>
+
+               <id>https://test.com/blog/test</id>
+
+               <author>
+                   <name><![CDATA[ Brent Roose ]]></name>
+               </author>
+
+               <summary type="html"><![CDATA[$title]]></summary>
+
+               <updated>2021-07-29T00:00:00+00:00</updated>
+           </entry>
+
+           <entry>
+               <title><![CDATA[$title]]></title>
+
+               <link rel="alternate" href="https://test.com/blog/test"/>
+
+               <id>https://test.com/blog/test</id>
+
+               <author>
+                   <name><![CDATA[ Brent Roose ]]></name>
+               </author>
+
+               <summary type="html"><![CDATA[$title]]></summary>
+
+               <updated>2021-07-29T00:00:00+00:00</updated>
+           </entry>
+       </feed>
+       XML;
     }
 }
