ca4ff62e0342f4d57a82ea59faa207bee76a72da
Vitaliy Semenov <synthex@bk.ru>
2024-05-06 09:34:28 +0500

    snapshot testing

--- tests/View/Components/LastSeenTest.php
+++ tests/View/Components/LastSeenTest.php
@@ -5,24 +5,23 @@ namespace Tests\View\Components;
 use App\Models\BlogPost;
 use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
+use Spatie\Snapshots\MatchesSnapshots;
 use Tests\TestCase;
 
 class LastSeenTest extends TestCase
 {
+    use MatchesSnapshots;
+
     public function test_test_last_seen(): void
     {
         $post = BlogPost::factory()->create();
 
         $this->travelTo(Carbon::make('2021-01-01'));
 
-        $this->blade('<x-last-seen :post="$post" />', compact('post'))
-            ->assertDontSee('Last seen')
-            ->assertDontSee('2021-01-01');
+        $this->assertMatchesSnapshot((string) $this->blade('<x-last-seen :post="$post" />', compact('post')));
 
         app(Request::class)->cookies->set("last_seen_{$post->slug}", now()->toDateTimeString());
 
-        $this->blade('<x-last-seen :post="$post" />', compact('post'))
-            ->assertSee('Last seen')
-            ->assertSee('2021-01-01');
+        $this->assertMatchesSnapshot((string) $this->blade('<x-last-seen :post="$post" />', compact('post')));
     }
 }
